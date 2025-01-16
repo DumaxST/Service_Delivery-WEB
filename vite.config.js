3cc122c7ac903d949c4fb65303180597ccae7895
nadirulmi <ulminadir@gmail.com>
2025-01-16 14:16:03 -0300

    add dotenv support and update project metadata for Dumax services

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({  
   plugins: [react()],
   build: {
     outDir: 'build', 
   },
+  define: {
+    'process.env': {
+      REACT_MICROSERVICES_API: JSON.stringify(process.env.REACT_MICROSERVICES_API),
+    },
+  }
 })
